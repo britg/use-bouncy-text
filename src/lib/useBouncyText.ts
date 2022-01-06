700782e39939509a00d3c1e5397f848a9373cd25
Brit Gardner <brit@britg.com>
2022-01-06 10:36:45 -0600

    feat: initial commit

---
+++ src/lib/useBouncyText.ts
@@ -0,0 +1,32 @@
+/* eslint-disable functional/immutable-data */
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+export function useBouncyText(ref: MutableRefObject<HTMLElement>) {
+  const appliedRef = useRef(false);
+  useEffect(() => {
+    if (appliedRef.current || !ref.current) {
+      return;
+    }
+    appliedRef.current = true;
+    const text = ref.current.innerHTML;
+    let wrappedText = '';
+
+    for (let i = 0; i < text.length; i++) {
+      const char = text.charAt(i);
+      if (char === ' ') {
+        wrappedText += '&nbsp;';
+      } else {
+        wrappedText += '<span>' + text.charAt(i) + '</span>';
+      }
+    }
+
+    ref.current.innerHTML = wrappedText;
+
+    const els = ref.current.getElementsByTagName('span');
+    for (let j = 0; j < els.length; j++) {
+      const el = els[j];
+      el.className = 'bouncy-text';
+      el.style.animationDelay = `${(j % 5) * 150}ms`;
+    }
+  }, [ref]);
+}
